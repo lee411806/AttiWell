747b1eb15ab90f72922ccf9c5dd4a02e26ef394b
zxcxz01 <zxcxz01@gmail.com>
2023-12-11 17:41:58 +0900

    영광_설정오류들수정_소은_설정오류들수정_전체상품페이징처리해결_2023_12_11_17:41_커밋

--- attiWell/src/main/java/com/attiWell/goods/controller/GoodsController.java
+++ attiWell/src/main/java/com/attiWell/goods/controller/GoodsController.java
@@ -1,6 +1,8 @@
 package com.attiWell.goods.controller;
 
 
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -9,9 +11,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 public interface GoodsController  {
-	public ModelAndView goodsDetail(@RequestParam("goods_id") String goods_id,HttpServletRequest request, HttpServletResponse response) throws Exception;
-	public @ResponseBody String keywordSearch(@RequestParam("keyword") String keyword,HttpServletRequest request, HttpServletResponse response) throws Exception;
-	public ModelAndView searchGoods(@RequestParam("searchWord") String searchWord,HttpServletRequest request, HttpServletResponse response) throws Exception;
-//	allGoodsList 추가	
-	public ModelAndView allGoodsList(HttpServletRequest request, HttpServletResponse response) throws Exception;
+   public ModelAndView goodsDetail(@RequestParam("goods_id") String goods_id,HttpServletRequest request, HttpServletResponse response) throws Exception;
+   public @ResponseBody String keywordSearch(@RequestParam("keyword") String keyword,HttpServletRequest request, HttpServletResponse response) throws Exception;
+   public ModelAndView searchGoods(@RequestParam("searchWord") String searchWord,HttpServletRequest request, HttpServletResponse response) throws Exception;
+//   allGoodsList 추가   
+   public ModelAndView allGoodsList(@RequestParam Map<String, String> dateMap, HttpServletRequest request, HttpServletResponse response) throws Exception;
 }
--- attiWell/src/main/java/com/attiWell/goods/service/GoodsService.java
+++ attiWell/src/main/java/com/attiWell/goods/service/GoodsService.java
@@ -7,12 +7,13 @@ import java.util.Map;
 import com.attiWell.goods.vo.GoodsVO;
 
 public interface GoodsService {
-	
-	public Map<String,List<GoodsVO>> listGoods() throws Exception;
-	public Map goodsDetail(String _goods_id) throws Exception;
-	
-	public List<String> keywordSearch(String keyword) throws Exception;
-	public List<GoodsVO> searchGoods(String searchWord) throws Exception;
-//	allGoodsList 추가	
-	public List<GoodsVO> allGoodsList() throws Exception;
+   
+   public Map<String,List<GoodsVO>> listGoods() throws Exception;
+//   categoryList 추가
+   public Map<String,List<GoodsVO>> categoryList() throws Exception;
+   public Map goodsDetail(String _goods_id) throws Exception;
+   public List<String> keywordSearch(String keyword) throws Exception;
+   public List<GoodsVO> searchGoods(String searchWord) throws Exception;
+//   allGoodsList 추가   
+   public List<GoodsVO> allGoodsList(Map condMap) throws Exception;
 }
